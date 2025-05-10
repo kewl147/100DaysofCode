394f35af0ec646bf46007c2dd0b78dafe3c9f161
Patrick Dillon <kewl147@gmail.com>
2025-05-10 14:51:38 -0500

    NA

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Lets figure out your band name.")
+city = input("What city did you grow up in?\n")
+petName = input("What is your pet's name?\n")
+print("Your Band name is " + city + " " + petName)

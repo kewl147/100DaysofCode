1600a8f38c9ee85bb5a45954287d13cc9ab9a14b
Patrick Dillon <kewl147@gmail.com>
2025-05-10 16:20:25 -0500

    NA

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -1,13 +1,17 @@
 print("Welcome to the tip calculator!\n")
+
+# inputs
 bill = float(input("What was the total bill? \n$"))
 tip = int(input("What percentage tip would you like to give? \n10 12 15 \n"))
 people = int(input("How many people to split the bill? \n"))
 
+# math
 tipTotal = bill * tip / 100
 totalBill = bill + tipTotal
 totalPerPerson = round(float((bill + tipTotal)/people), 2)
 mismatch = round((totalPerPerson * people) - totalBill, 2)
 
+#outputs
 print(f"Your total bill with tip is ${totalBill:.2f}")
 print(f"Each person should pay ${totalPerPerson:.2f}")
 print(f"There will be a mismatch of ${mismatch:.2f}")

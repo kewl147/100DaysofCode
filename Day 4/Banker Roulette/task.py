e7991694c479fc8bd7410860a3a347f4c0e5d376
kewl1 <kewl147@gmail.com>
2025-05-26 21:19:22 -0500

    NA

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,11 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+numberfriend = random.randint(0,4)
+
+print(numberfriend)
+
+print(friends[numberfriend])
+
+print(random.choice(friends))
---
+++ Day 4/Random Module/task.py
@@ -0,0 +1,20 @@
+import random
+import my_module
+
+number = random.randint(1,500)
+
+print(number)
+
+print(my_module.myFavoriteNumber)
+
+turkey = random.random() / random.random()
+
+print(turkey)
+
+bird = random.uniform(1,500)
+print(bird)
+
+if turkey > 1:
+    print("Heads")
+else:
+    print("Tails")

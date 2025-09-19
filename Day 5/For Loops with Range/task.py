dec5852203764c38d1533576ecb7adf3b69ccbf4
Patrick Dillon <kewl147@gmail.com>
2025-09-19 17:34:31 -0500

    NA

--- Day 5/For Loops with Range/task.py
+++ Day 5/For Loops with Range/task.py
@@ -4,3 +4,13 @@ for number in range(1, 101):
     total += number
 
 print(total)
+
+for number in range(1, 101):  # goes from 1 up to 100 inclusive
+    if number % 3 == 0 and number % 5 == 0:  # divisible by both 3 and 5
+        print("FizzBuzz")
+    elif number % 3 == 0:  # divisible by 3
+        print("Fizz")
+    elif number % 5 == 0:  # divisible by 5
+        print("Buzz")
+    else:
+        print(number)  # not divisible by 3 or 5, just print the number
